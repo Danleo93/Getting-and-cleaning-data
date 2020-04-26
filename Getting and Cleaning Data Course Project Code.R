5aeb92dbfc90528d04982334e2ba28699bdfe436
danleo93 <daniele.leo93@gmail.com>
2020-04-26 13:00:21 +0200

    3th commit

--- Getting and Cleaning Data Course Project Code.R
+++ Getting and Cleaning Data Course Project Code.R
@@ -1,4 +1,4 @@
-newwd<-setwd("insert the path of your WD containing the data for this assignment")
+newwd<-setwd("C:/Users/danie/Desktop/Coursera/course 3 cleaning data/week4 file peer/files peer" )
 features<-read.table("features.txt")
 activities<-read.table("activity_labels.txt")  #I read all the files that will be helpfull to assign the right variables name and acitivity informations
 
@@ -21,12 +21,14 @@ test$subject<-subject_test$V1 #I include a new column with the subjects informat
 test$activity<-y_test$V1 #I include a new column with the activity informations to test data frame
 
 mydata<-rbind(train,test) #I merge the two data frames (STEP1)
-mydata$activity<-as.factor(mydata$activity) 
-levels(mydata$activity)<-as.character(activities$V2) #I assign the right esplicative activity labels to the column that contains the activities code (STEP3)
 
 col_mean_std<-grep(pattern = "mean\\(\\)|std\\(\\)|activity|subject",x=names(mydata)) #I obtain the column index of column named with std, mean, subject or activity
 data_mean_std<-mydata[,col_mean_std] #I use this index to extract the right data (STEP2)
+data_mean_std$activitylabels<-as.factor(mydata$activity)
+levels(data_mean_std$activitylabels)<-as.character(activities$V2) #I assign the right esplicative activity labels to the column that contains the activities code (STEP3)
+
+
 library(reshape2)
-melted_data<-melt(data_mean_std,id=c("subject","activity"),measure.vars = names(data_mean_std[,1:66])) #I melt subject and activity data 
-tidydata<-dcast(melted_data,subject~activity,mean) #I obtain my tidy data containing the mean for each subject for each activity
+melted_data<-melt(data_mean_std,id=c("subject","activitylabels"),measure.vars = names(data_mean_std[,1:66])) #I melt subject and activity data 
+tidydata<-dcast(melted_data,activitylabels+subject~variable,mean) #I obtain my tidy data containing the mean for each subject for each activity
 write.table(tidydata,file="tidydata.txt", row.names = F) #I wirte this data in a txt table
